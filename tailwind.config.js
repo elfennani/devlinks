63a6778b4163929ac8a287b909663b001be1d3ac
Nizar Elfennani <elfennani2002@gmail.com>
2024-03-03 21:52:24 +0100

    Added colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: {
+        bold: "#633CFF",
+        normal: "#BEADFF",
+        light: "#EFEBFF",
+      },
+      secondary: {
+        bolder: "#333333",
+        bold: "#737373",
+        normal: "#D9D9D9",
+        light: "#FAFAFA",
+      },
+      error: "#FF3939",
+    },
     extend: {},
   },
   plugins: [],
