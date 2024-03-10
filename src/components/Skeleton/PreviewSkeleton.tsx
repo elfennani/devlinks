fb0eb93ebba451c64bde7ea5a198cf369e7cbd1f
Nizar Elfennani <elfennani2002@gmail.com>
2024-03-10 18:12:42 +0000

    Adjusted contrast for preview skeleton

--- src/components/Skeleton/PreviewSkeleton.tsx
+++ src/components/Skeleton/PreviewSkeleton.tsx
@@ -12,14 +12,14 @@ const PreviewSkeleton = ({ uuid }: { uuid: string }) => {
           className="size-28 border-4 border-primary-bold rounded-full bg-neutral-100 object-cover self-center"
         />
         <div className="flex flex-col items-center gap-2">
-          <div className="h-8 w-72 bg-graphite-light rounded-lg animate-pulse" />
-          <div className="h-4 w-52 bg-graphite-light rounded-md"></div>
+          <div className="h-8 w-72 bg-graphite-normal rounded-lg animate-pulse" />
+          <div className="h-4 w-52 bg-graphite-normal rounded-md"></div>
         </div>
         <div className="flex flex-col gap-4">
-          <div className="h-12 w-full bg-graphite-light rounded-lg animate-pulse" />
-          <div className="h-12 w-full bg-graphite-light rounded-lg animate-pulse" />
-          <div className="h-12 w-full bg-graphite-light rounded-lg animate-pulse" />
-          <div className="h-12 w-full bg-graphite-light rounded-lg animate-pulse" />
+          <div className="h-12 w-full bg-graphite-normal rounded-lg animate-pulse" />
+          <div className="h-12 w-full bg-graphite-normal rounded-lg animate-pulse" />
+          <div className="h-12 w-full bg-graphite-normal rounded-lg animate-pulse" />
+          <div className="h-12 w-full bg-graphite-normal rounded-lg animate-pulse" />
         </div>
       </div>
     </div>
